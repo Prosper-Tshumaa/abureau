8b888f05c9ce1bcb29c2ced84bdedfcd1995cb82
Prosper Dante <tshumaprosper11@gmail.com>
2022-09-11 11:35:37 +0200

    Addition

--- resources/views/pages/coaching.blade.php
+++ resources/views/pages/coaching.blade.php
@@ -35,35 +35,34 @@
 <body>
     @include('partials._navbar')
     <div class="bg-white py-6 sm:py-8 lg:py-12">
-        <div class="max-w-screen-xl px-4 md:px-8 mx-auto">
-          <div class="grid md:grid-cols-2 gap-8 lg:gap-12">
-            <div>
-              <div class="h-64 md:h-auto bg-gray-100 overflow-hidden rounded-lg shadow-lg">
-                <img src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600&h=750" loading="lazy" alt="Photo by Martin Sanchez" class="w-full h-full object-cover object-center" />
-              </div>
-            </div>
-
-            <div class="md:pt-8">
-              <p class="text-indigo-500 font-bold text-center md:text-left">Who we are</p>
-
-              <h1 class="text-gray-800 text-2xl sm:text-3xl font-bold text-center md:text-left mb-4 md:mb-6">Our competitive advantage</h1>
-
-              <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
-                At Assessment Bureau we understand that in this modern world, at times it's difficult for students to balance their time between classes, studying, writting assignments and running their entrepreneural businesses or freelancing jobs. <br /><br />
-
-                Hence we have developed a model of learning through Assignments, quiz or digital assessments. Instead of students being overwhelmed with their school work and spending more time studying resources and researching they can utilise some of the time on their entrepreneurship work whilst our team ofloads their workload by providing quality solutions tailored to their specific needs.
-
-                Send us your assessment and we quote you for free within the hour......
-              </p>
-
-              <h2 class="text-gray-800 text-xl sm:text-2xl font-semibold text-center md:text-left mb-2 md:mb-4">About us</h2>
-
-              <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">Assessment Bureau is an online service provider of Excellent tailored Solutions to tertiary education assignments, digital assessments, dissertations , graphical representations and numerical problems.
-                We specialise in the field of Commerce covering from compulsory  subjects / modules such as Financial Accounting ( FAC) , Management Accounting (MAC),  Business Management ( BNU), Micro & Macro Economics ( ECS) , Human Resources Management (HRM) Introductory to Financial Mathematics, to elective modules including but not limited to Risk Management, Strategic Management, Econometrics, Business Information Systems, Business computing,  Statistics, etc.
-                Our team of tutors and on-the-field professionals  posses  different expertise in these subjects and are experienced in handling any kind of assignments in accordance to specific needs of our students.
-                Not only do we render these services , our team is also available 24/7 to assist students in preparing for exams through virtual lessons that focus on key concepts and models.</p>
-            </div>
+        <div class="max-w-screen-md px-4 md:px-8 mx-auto">
+          <h1 class="text-gray-800 text-2xl sm:text-3xl font-bold text-center mb-4 md:mb-6">Our competitive advantage</h1>
+      
+          <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
+            This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing. Filler text is dummy text which has no meaning however looks very similar to real text. The important factor when using filler text is that the text looks realistic otherwise it will not look very good.<br /><br />
+      
+            This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is <a href="#" class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 underline transition duration-100">random</a> or otherwise generated. It may be used to display a sample of fonts or generate text for testing. Filler text is dummy text which has no meaning however looks very similar to real text.
+          </p>
+      
+          <h2 class="text-gray-800 text-xl sm:text-2xl font-semibold mb-2 md:mb-4">About us</h2>
+      
+          <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing. Filler text is dummy text which has no meaning however looks very similar to real text.</p>
+      
+          <ul class="list-disc list-inside text-gray-500 sm:text-lg mb-6 md:mb-8">
+            <li>This is a section of some simple filler text</li>
+            <li>Also known as placeholder text</li>
+            <li>It shares some characteristics of a real written text</li>
+          </ul>
+      
+          <blockquote class="text-gray-500 sm:text-lg italic border-l-4 pl-4 md:pl-6 mb-6 md:mb-8">“This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.”</blockquote>
+      
+          <div class="bg-gray-100 overflow-hidden rounded-lg shadow-lg relative mb-6 md:mb-8">
+            <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600&h=350" loading="lazy" alt="Photo by Minh Pham" class="w-full h-full object-cover object-center" />
           </div>
+      
+          <h2 class="text-gray-800 text-xl sm:text-2xl font-semibold mb-2 md:mb-4">Features</h2>
+      
+          <p class="text-gray-500 sm:text-lg">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing. Filler text is dummy text which has no meaning however looks very similar to real text.</p>
         </div>
       </div>
     @include('partials._footer')
